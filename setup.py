a8d41565c5e41dbe85716f4d4d9c6fb485a11c11
Miguel Ballesteros <miballegh@outlook.com>
2020-04-24 16:01:58 +0200

    Fixed namespace and setup package finding

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.rst", "r") as fh:
     long_desc = fh.read()
@@ -13,7 +13,7 @@ setup(
     author_email='miguel.ballesteros@dowjones.com',
     # Warning: the folder 'factiva' should NOT have an __init__.py file to avoid conflicts with the same namespace across other packages
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=['factiva.news'],
     # package_data={'': ['*.csv']},
     url='https://github.com/dowjones/factiva-news-python',
 
