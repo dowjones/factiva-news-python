e640f97826aa6abdac77ec71d64ed2ec32ef5b4f
Miguel Ballesteros <miballegh@outlook.com>
2020-04-24 16:04:18 +0200

    Manual snapshot objects test

---
+++ test/analytics.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from factiva.news import Snapshot
+from factiva.news.snapshot import AnalyticsJob
+
+querystr = "publication_datetime >= '2018-01-01 00:00:00' AND publication_datetime <= '2018-01-10 00:00:00' AND LOWER(language_code) = 'en'"
+st1 = Snapshot(query=querystr)
+
+# st1.submit_analytics_job()
+
+# historic_analytics_job = AnalyticsJob()
+# historic_analytics_job.job_id = '60a0bb91-3b94-4f54-b8b9-432b72b9315e'
+# historic_analytics_job.job_state = 'JOB_CREATED'
+# historic_analytics_job.submitted_datetime = datetime.now()
+# historic_analytics_job.link = 'https://api.dowjones.com/alpha/analytics/60a0bb91-3b94-4f54-b8b9-432b72b9315e'
+# historic_analytics_job.data = []
+
+# st1.last_analytics_job = historic_analytics_job
+# st1.get_analytics_job_results()
+
+analytics_data = st1.process_analytics()
+
+print('Done!')
--- test/explain.py
+++ test/explain.py
@@ -1,4 +1,21 @@
+from datetime import datetime
 from factiva.news import Snapshot
 
-st = Snapshot('abcd1234abcd1234abcd1234abcd1234')
-print(st)
+querystr = "publication_datetime >= '2018-01-01 00:00:00' AND publication_datetime <= '2018-01-10 00:00:00' AND LOWER(language_code) = 'en'"
+st1 = Snapshot(query=querystr)
+
+# st1.submit_explain_job()
+
+# historic_explain_job = {
+#     'job_id': '8ce4389c-dec9-4cd9-b9fc-8193881b4430',
+#     'job_state': 'JOB_CREATED',
+#     'submitted_datetime': datetime.now(),
+#     'link': 'https://api.dowjones.com/alpha/extractions/documents/8ce4389c-dec9-4cd9-b9fc-8193881b4430/_explain',
+#     'document_volume': 0
+# }
+# st1.last_explain_job = historic_explain_job
+# st1.get_explain_job_results()
+
+st1.process_explain()
+
+print('Done!')
