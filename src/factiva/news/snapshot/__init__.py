f88b7ed25c02c8bd49a2cfa68cf6b4cd6c2882bd
Miguel Ballesteros <miballegh@outlook.com>
2021-07-22 20:39:08 +0200

    Fixed modules ennumeration

--- src/factiva/news/snapshot/__init__.py
+++ src/factiva/news/snapshot/__init__.py
@@ -1,6 +1,7 @@
 """Define methods and properties for Snapshot class."""
-__all__ = ['query', 'jobs', 'snapshot']
-
 from .query import SnapshotQuery
 from .jobs import AnalyticsJob, ExplainJob, ExtractionJob, UpdateJob
 from .snapshot import Snapshot
+from .files import SnapshotFiles
+
+__all__ = [Snapshot, SnapshotQuery, SnapshotFiles, AnalyticsJob, ExplainJob, ExtractionJob, UpdateJob, 'query', 'jobs', 'snapshot', 'files']
--- src/factiva/news/taxonomy/__init__.py
+++ src/factiva/news/taxonomy/__init__.py
@@ -1,2 +1,4 @@
 """Define methods and properties for Taxonomy class."""
 from .taxonomy import Taxonomy
+
+__all__ = [Taxonomy]
