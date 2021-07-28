cb7b88b37232a36d0b4f81d643072f202c2daf67
Miguel Ballesteros <miballegh@outlook.com>
2021-07-28 15:45:03 +0200

    Refactored __all__

--- src/factiva/news/__init__.py
+++ src/factiva/news/__init__.py
@@ -1,9 +1,14 @@
 
-"""Init file for news module."""
+"""
+    Init file for news module.
+"""
+__all__ = ['snapshot', 'stream', 'bulknews', 'taxonomy',
+    'Snapshot', 'SnapshotQuery', 'SnapshotFiles', 'Stream',
+    'Listener', 'Subscription', 'Taxonomy']
+
 from .__version__ import __version__
 from .snapshot import (Snapshot, SnapshotQuery, SnapshotFiles)
 from .stream import (Stream, Listener, Subscription)
 from .taxonomy.taxonomy import Taxonomy
 
-__all__ = [Snapshot, SnapshotQuery, SnapshotFiles, Stream, Listener, Subscription, Taxonomy, 'snapshot', 'stream', 'bulknews', 'taxonomy']
 version = __version__
--- src/factiva/news/snapshot/__init__.py
+++ src/factiva/news/snapshot/__init__.py
@@ -1,7 +1,11 @@
-"""Define methods and properties for Snapshot class."""
+"""
+    Define methods and properties for Snapshot class.
+"""
+__all__ = ['Snapshot', 'SnapshotQuery', 'SnapshotFiles',
+    'AnalyticsJob', 'ExplainJob', 'ExtractionJob', 'UpdateJob',
+    'query', 'jobs', 'snapshot', 'files']
+
 from .query import SnapshotQuery
 from .jobs import AnalyticsJob, ExplainJob, ExtractionJob, UpdateJob
 from .snapshot import Snapshot
 from .files import SnapshotFiles
-
-__all__ = [Snapshot, SnapshotQuery, SnapshotFiles, AnalyticsJob, ExplainJob, ExtractionJob, UpdateJob, 'query', 'jobs', 'snapshot', 'files']
