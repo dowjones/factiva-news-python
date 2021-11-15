5071948750d9ec88c4de4f7b95f8f44573c322d6
lonly7star <40080387+lonly7star@users.noreply.github.com>
2021-11-14 21:17:38 -0600

    adding the Environment Variables as readme instructed for faster testing setup

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+import os
+os.environ["FACTIVA_USERKEY"] = 'abcd1234abcd1234abcd1234abcd1234'
+os.environ["FACTIVA_SNAPSHOTID"] = 'pdsdakabfd'
+os.environ["FACTIVA_SNAPSHOT_UPDATEID"] = 'pdsdakabfd-additions-20210513060214'
+os.environ["FACTIVA_STREAMID"] = 'hnrvryxkpo'
